335f0142638cc120363c9f9cb7ed9c5f30958a9b
Datskov Andrey <andrey.datskov@gmail.com>
2024-10-09 22:15:51 +1000

    separate product and service

--- Core/Service.cs
+++ Core/Service.cs
@@ -1,13 +1,21 @@
 ﻿namespace Eshop.Core
 {
-    public class Service : Product
+    public class Service
     {
-        public Service(int Id, string Name) : base(Id, Name)
-        {
-        }
+        public int Id { get; }
+        public string Name { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public uint Stock { get; set; }
 
-        public Service(int Id, string Name, string Description) : base(Id, Name, Description)
+        public Service(int Id, string Name)
         {
+            this.Id = Id;
+            this.Name = Name;
+        }
+        public Service(int Id, string Name, string Description) : this(Id, Name)
+        {
+            this.Description = Description;
         }
     }
 }
